bc074e882aafd179fcc35e6f40236d421561a4ea
jysun <1650949260@qq.com>
2021-08-12 12:04:48 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   run_CMPP_anno.sh

--- run_CMPP_anno.sh
+++ run_CMPP_anno.sh
@@ -15,8 +15,8 @@ done
 
 blastp -query $FASTA -db $PATH_TO_FUNCDB/merops/merops_scan.lib -evalue 1e-10 -outfmt 6 -max_target_seqs 5 -num_threads 4 -out $FASTA.merops.btb
 blastp -query $FASTA -db $PATH_TO_FUNCDB/PHI/phi-base_current.fas -evalue 1e-10 -outfmt 6 -max_target_seqs 5 -num_threads 4 -out $FASTA.phi.btb
-hmmscan --tblout $FASTA.p450.htb --noali --cpu 4 -E 1e-5 $PATH_TO_FUNCDB/P450/P450.hmm.txt $FASTA
-hmmscan --tblout $FASTA.cazy.htb --noali --cpu 4 -E 1e-5 $PATH_TO_FUNCDB/CAZy/dbCAN-HMMdb-V9.txt $FASTA
+hmmscan -o P450.out --tblout $FASTA.p450.htb --noali --cpu 4 -E 1e-5 $PATH_TO_FUNCDB/P450/P450.hmm.txt $FASTA
+hmmscan -o CAZy.out --tblout $FASTA.cazy.htb --noali --cpu 4 -E 1e-5 $PATH_TO_FUNCDB/CAZy/dbCAN-HMMdb-V9.txt $FASTA
 
 grep -v "#" $FASTA.p450.htb|awk -F " " '{print$3}'|sort|uniq > p450.glist
 grep -v "#" $FASTA.cazy.htb|awk -F " " '{print$3}'|sort|uniq > cazy.glist
@@ -27,6 +27,8 @@ mkdir CMPP_out
 mv *glist CMPP_out
 mv *htb CMPP_out
 mv *btb CMPP_out
+rm P450.out
+rm CAZy.out
 
 echo "Done Blastp and hmmscan!"
 
